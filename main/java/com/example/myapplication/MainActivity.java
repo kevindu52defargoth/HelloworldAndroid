2acf94e1f87a8753def22640c13a37181f730185
kevindu52defargoth <>
2021-10-16 18:20:32 +0200

    identification des elements d'interface dans le code de MainActivity

--- main/java/com/example/myapplication/MainActivity.java
+++ main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView mGreetingTextView;
+
+    private EditText mNameEditText;
+
+    private Button mPlayButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mGreetingTextView = findViewById(R.id.main_textview);
+        mNameEditText = findViewById(R.id.main_edittext);
+        mPlayButton = findViewById(R.id.main_button);
     }
 }
